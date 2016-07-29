1d40d9298b92585db549dcd293cf8cc0e932d6b2
Karl Benedict <kbene@unm.edu>
2016-07-29 08:09:51 -0600

    no message

--- CV/generate.sh
+++ CV/generate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pandoc -s --template=./default.latex -o cv.pdf CV.md
-pandoc -s  -o cv.pdf CV.md
+# pandoc -s  -o cv.pdf CV.md
+pandoc -s --self-contained --css=cv.css -o index.html CV.md
 pandoc -s --self-contained --css=cv.css -o cv.html CV.md
 pandoc -s --reference-docx=template.docx -o cv.docx CV.md
 pandoc -s --epub-stylesheet=cv.css -o cv.epub CV.md
